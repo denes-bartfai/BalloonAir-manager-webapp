cd900843dd7f2ae9963e87b378a509f26a47e338
denes-bartfai <denes.bartfai@gmail.com>
2024-04-17 10:35:13 +0200

    Create Performance model

---
+++ server/Model/PerformanceModel.cs
@@ -0,0 +1,12 @@
+﻿namespace server.Model;
+
+public class PerformanceModel
+{
+    public int Id { get; init; }
+    public DateTime  Date { get; init; }
+    public string State { get; init; }
+    public string City { get; init; }
+    public string Event { get; init; }
+    public decimal Sales { get; init; }
+    public string Comment { get; init; }
+}
