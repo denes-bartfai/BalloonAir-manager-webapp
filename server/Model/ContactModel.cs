8683d8bcb385324e8931a932a8e24a49a6c4f677
denes-bartfai <denes.bartfai@gmail.com>
2024-04-17 10:43:42 +0200

    Create Contact model

---
+++ server/Model/ContactModel.cs
@@ -0,0 +1,12 @@
+﻿namespace server.Model;
+
+public class ContactModel
+{
+    public int Id { get; init; }
+    public string City { get; init; }
+    public string Company { get; init; }
+    public string Position { get; init; }
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string PhoneNumber { get; init; }
+}
