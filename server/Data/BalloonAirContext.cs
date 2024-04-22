f56a7ec5c806ed1055f9f2ca658b7346c47c9849
denes-bartfai <denes.bartfai@gmail.com>
2024-04-22 14:59:08 +0200

    Create DbContext

---
+++ server/Data/BalloonAirContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Model;
+
+namespace server.Data;
+
+public class BalloonAirContext : DbContext
+{
+    public BalloonAirContext(DbContextOptions<BalloonAirContext> options) : base(options)
+    {
+    }
+    public DbSet<PerformanceModel> Performance { get; set; }
+    public DbSet<ContactModel> Contact { get; set; }
+}
