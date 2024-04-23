6b53590b7645f0ae327ef7f9ee85cdbf0f087b3d
denes-bartfai <denes.bartfai@gmail.com>
2024-04-23 19:07:31 +0200

    Upgrade and customize Contact model

--- server/Model/ContactModel.cs
+++ server/Model/ContactModel.cs
@@ -1,12 +1,38 @@
-﻿namespace server.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Model;
 
 public class ContactModel
 {
     public int Id { get; init; }
-    public string City { get; init; }
-    public string Company { get; init; }
-    public string Position { get; init; }
-    public string Name { get; init; }
-    public string Email { get; init; }
-    public string PhoneNumber { get; init; }
+    
+    [Required]
+    public string City { get; set; }
+
+    [Required]
+    public string Company { get; set; }
+
+    private string _position = "ügyintéző";
+
+    public string Position
+    {
+        get => _position;
+        set => _position = value?.ToLowerInvariant() ?? "ügyintéző";
+    }
+    
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    public ContactModel()
+    {
+        Company = $"{City} Önkormányzat";
+    }
 }
