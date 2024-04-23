9bf68b8f2d3fa7e78ccce0e2d2b7ea2528c1e80d
denes-bartfai <denes.bartfai@gmail.com>
2024-04-23 19:08:51 +0200

    Upgrade and customize the Performance model

--- server/Model/PerformanceModel.cs
+++ server/Model/PerformanceModel.cs
@@ -1,12 +1,25 @@
-﻿namespace server.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Model;
 
 public class PerformanceModel
 {
     public int Id { get; init; }
-    public DateTime  Date { get; init; }
-    public string State { get; init; }
-    public string City { get; init; }
-    public string Event { get; init; }
-    public int Sales { get; init; }
-    public string Comment { get; init; }
+    
+    [Required]
+    public DateTime  Date { get; set; }
+
+    public int Year => Date.Year;
+    public int Month => Date.Month;
+    public int Day => Date.Day;
+    
+    [Required]
+    public string State { get; set; }
+    [Required]
+    public string City { get; set; }
+    [Required]
+    public string Event { get; set; }
+    [Required]
+    public int Sales { get; set; }
+    public string Comment { get; set; }
 }
