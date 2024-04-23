cecf0cb29d9da83ca291cdfee8c080bf795a1f85
denes-bartfai <denes.bartfai@gmail.com>
2024-04-23 13:10:15 +0200

    Create IPerformanceRepository

---
+++ server/Service/IPerformanceRepository.cs
@@ -0,0 +1,18 @@
+﻿using server.Model;
+
+namespace server.Service;
+
+public interface IPerformanceRepository
+{
+    Task<IEnumerable<PerformanceModel>> GetAll();
+
+    Task<PerformanceModel?> GetByName(string name);
+
+    Task<PerformanceModel?> GetById(int id);
+
+    void Add(PerformanceModel performance);
+
+    void Delete(PerformanceModel performance);
+
+    Task<PerformanceModel> Update(int id, PerformanceModel performance);
+}
