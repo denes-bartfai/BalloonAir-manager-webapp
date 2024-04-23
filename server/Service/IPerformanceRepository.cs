2e926829bd205347c0421b962d0e545858f91da9
denes-bartfai <denes.bartfai@gmail.com>
2024-04-23 19:08:21 +0200

    Make the Performance interface completely async

--- server/Service/IPerformanceRepository.cs
+++ server/Service/IPerformanceRepository.cs
@@ -4,15 +4,15 @@ namespace server.Service;
 
 public interface IPerformanceRepository
 {
-    Task<IEnumerable<PerformanceModel>> GetAll();
+    Task<List<PerformanceModel>> GetAll();
 
     Task<PerformanceModel?> GetByName(string name);
 
     Task<PerformanceModel?> GetById(int id);
 
-    void Add(PerformanceModel performance);
+    Task Add(PerformanceModel performance);
 
-    void Delete(PerformanceModel performance);
+    Task Delete(PerformanceModel performance);
 
     Task<PerformanceModel> Update(int id, PerformanceModel performance);
 }
