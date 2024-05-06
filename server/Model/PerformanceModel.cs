b39ca7c8369322ed732d324b849c9ff970d5aded
denes-bartfai <denes.bartfai@gmail.com>
2024-05-06 20:48:14 +0200

    Take out the required attribute from PerformanceModel

--- server/Model/PerformanceModel.cs
+++ server/Model/PerformanceModel.cs
@@ -5,21 +5,10 @@ namespace server.Model;
 public class PerformanceModel
 {
     public int Id { get; init; }
-    
-    [Required]
     public DateTime  Date { get; set; }
-
-    public int Year => Date.Year;
-    public int Month => Date.Month;
-    public int Day => Date.Day;
-    
-    [Required]
     public string State { get; set; }
-    [Required]
     public string City { get; set; }
-    [Required]
     public string Event { get; set; }
-    [Required]
     public int Sales { get; set; }
     public string Comment { get; set; }
 }
