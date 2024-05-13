ef4437a182ca67432ade7698f09ff0a1ea6d3e47
denes-bartfai <denes.bartfai@gmail.com>
2024-05-13 22:11:39 +0200

    Create Contact component

--- client/src/Components/Contacts/Contacts.jsx
+++ client/src/Components/Contacts/Contacts.jsx
@@ -1,11 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 
 import "./Contacts.css";
 
+
 const Contacts = () => {
+
+  const [contactData, setContactData] = useState([]);
+
+  useEffect(() =>{
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("/api/Contact/GetAllContact");
+      if (!res.ok) {
+        throw new Error(`Network response was not ok: ${res.statusText}`);
+      }
+      const data = await res.json();
+      setContactData(data.res); // Extract "res" property
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setContactData([]); // Set performanceData to an empty array on error
+    }
+  };
+
+  
+
   return (
-    <div>Contacts</div>
-  )
+    <div className="contact-container">
+      <h2>Kontaktlista</h2>
+      <div className="card">
+        {contactData.length > 0 ? (
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead className="table-head">
+                <TableRow>
+                  <TableCell>Város</TableCell>
+                  <TableCell>Intémény</TableCell>
+                  <TableCell>Pozíció</TableCell>
+                  <TableCell>Név</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Telefonszám</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {contactData.map((contact, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{contact.city}</TableCell>
+                    <TableCell>{contact.company}</TableCell>
+                    <TableCell>{contact.position}</TableCell>
+                    <TableCell>{contact.name}</TableCell>
+                    <TableCell>{contact.email}</TableCell>
+                    <TableCell>{contact.phoneNumber}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <p>No contact available.</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Contacts
