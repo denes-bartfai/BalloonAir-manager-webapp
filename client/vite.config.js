0a0cde80f8825709e72bc4d599c1ad2079ee25c1
denes-bartfai <denes.bartfai@gmail.com>
2024-05-13 22:12:40 +0200

    Create a proxy server

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      port: 5173,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5269',
+          changeOrigin: true,
+          secure: false, 
+          withCredentials: true,
+        },
+      },
+    },
+  };
+});
