23b179b965e2c49619eb868a573cfa2a86fcebab
denes-bartfai <denes.bartfai@gmail.com>
2024-05-21 11:44:34 +0200

    Implement pagination for contact list

--- client/src/Components/Contacts/Contacts.jsx
+++ client/src/Components/Contacts/Contacts.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TextField, IconButton } from "@mui/material";
+import {NavigateBefore, NavigateNext, FirstPage, LastPage} from "@mui/icons-material";
 
 import "./Contacts.css";
 
@@ -7,6 +8,8 @@ import "./Contacts.css";
 const Contacts = () => {
 
   const [contactData, setContactData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
 
   useEffect(() =>{
     fetchData();
@@ -26,16 +29,57 @@ const Contacts = () => {
     }
   };
 
+  const totalPage = Math.ceil(contactData.length / itemsPerPage);
+
+  const handleClickNext = () => {
+    setCurrentPage(currentPage + 1)
+  };
+
+  const handleClickPrev = () => {
+    setCurrentPage(currentPage - 1)
+  };
+
+  const handleGoToPage = (event) => {
+    let pageNumber = parseInt(event.target.value);
+    if(!isNaN(pageNumber) && pageNumber >= 1 && pageNumber <= totalPage){
+      setCurrentPage(pageNumber);
+    }
+  };
+
+  const handleFirstPage = () => {
+    setCurrentPage(1);
+  }
+
+  const handleLastPage = () => {
+    setCurrentPage(totalPage);
+  };
+
+const renderData = () => {
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  return contactData.slice(startIndex, endIndex).map((contact, index) => (
+    <TableRow key={index}>
+      <TableCell>{contact.city}</TableCell>
+      <TableCell>{contact.company}</TableCell>
+      <TableCell>{contact.position}</TableCell>
+      <TableCell>{contact.name}</TableCell>
+      <TableCell>{contact.email}</TableCell>
+      <TableCell>{contact.phoneNumber}</TableCell>
+    </TableRow>
+  ))
+};
   
 
   return (
     <div className="contact-container">
       <h2>Kontaktlista</h2>
-      <div className="card">
+      <div className="contact-card">
         {contactData.length > 0 ? (
+          <div>
           <TableContainer component={Paper}>
             <Table>
-              <TableHead className="table-head">
+              <TableHead className="contact-table-head">
                 <TableRow>
                   <TableCell>Város</TableCell>
                   <TableCell>Intémény</TableCell>
@@ -46,19 +90,34 @@ const Contacts = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {contactData.map((contact, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{contact.city}</TableCell>
-                    <TableCell>{contact.company}</TableCell>
-                    <TableCell>{contact.position}</TableCell>
-                    <TableCell>{contact.name}</TableCell>
-                    <TableCell>{contact.email}</TableCell>
-                    <TableCell>{contact.phoneNumber}</TableCell>
-                  </TableRow>
-                ))}
+                {renderData()}
               </TableBody>
             </Table>
           </TableContainer>
+          <div className="contact-pagination-container">
+              <IconButton onClick={handleFirstPage} disabled={currentPage === 1}>
+                <FirstPage />
+              </IconButton>
+              <IconButton onClick={handleClickPrev} disabled={currentPage === 1}>
+              <NavigateBefore />
+              </IconButton>
+              <span>{`${currentPage} / ${totalPage}`}</span>
+              <IconButton onClick={handleClickNext} disabled={currentPage === totalPage}>
+              <NavigateNext />
+              </IconButton>
+              <IconButton onClick={handleLastPage} disabled={currentPage === totalPage}>
+              <LastPage />
+              </IconButton>
+              <TextField
+              type="number"
+              variant="outlined"
+              size="small"
+              label="Oldal"
+              onChange={handleGoToPage}
+              inputProps={{min: 1, max: totalPage}}
+              />
+            </div>
+          </div>
         ) : (
           <p>No contact available.</p>
         )}
