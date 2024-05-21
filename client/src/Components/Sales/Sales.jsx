9b959ae417a36e810ac5aa8386ce1021f06d7627
denes-bartfai <denes.bartfai@gmail.com>
2024-05-21 09:55:18 +0200

    Implement pagination for performance list

--- client/src/Components/Sales/Sales.jsx
+++ client/src/Components/Sales/Sales.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
-
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TextField, IconButton } from "@mui/material";
+import {NavigateBefore, NavigateNext, FirstPage, LastPage} from "@mui/icons-material";
 
 import "./Sales.css";
 
 const Sales = () => {
 
   const [performanceData, setPerformanceData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
 
   useEffect(() =>{
     fetchData();
@@ -19,27 +21,66 @@ const Sales = () => {
         throw new Error(`Network response was not ok: ${res.statusText}`);
       }
       const data = await res.json();
-      setPerformanceData(data.res); // Update state with the response data
+      setPerformanceData(data.res); 
     } catch (error) {
       console.error("Error fetching data:", error);
-      // Check if there's response text and log it
       if (error.response && error.response.text) {
         const responseText = await error.response.text();
         console.error("Response text:", responseText);
       }
-      // Set performanceData to an empty array on error
       setPerformanceData([]);
     }
   };
 
-  
+  const totalPage = Math.ceil(performanceData.length / itemsPerPage);
+
+  const handleClickNext = () => {
+    setCurrentPage(currentPage + 1)
+  };
+
+  const handleClickPrev = () => {
+    setCurrentPage(currentPage - 1)
+  };
+
+  const handleGoToPage = (event) => {
+    let pageNumber = parseInt(event.target.value);
+    if(!isNaN(pageNumber) && pageNumber >= 1 && pageNumber <= totalPage){
+      setCurrentPage(pageNumber);
+    }
+  };
+
+  const handleFirstPage = () => {
+    setCurrentPage(1);
+  };
+
+  const handleLastPage = () => {
+    setCurrentPage(totalPage);
+  }
+
+  const renderData = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return performanceData.slice(startIndex, endIndex).map((performance, index) => (
+      <TableRow key={index}>
+        <TableCell>{performance.date.substring(0, 10)}</TableCell>
+        <TableCell>{performance.state}</TableCell>
+        <TableCell>{performance.city}</TableCell>
+        <TableCell>{performance.event}</TableCell>
+        <TableCell>{performance.sales.toLocaleString()}</TableCell>
+        <TableCell>{performance.comment}</TableCell>
+        <TableCell></TableCell>
+        <TableCell></TableCell>
+    </TableRow>
+    ))
+  };
 
   return (
     <div className="sales-container">
       <h2>Értékesítés</h2>
       <div className="card">
         {performanceData.length > 0 ? (
-          <TableContainer component={Paper}>
+          <div>
+          <TableContainer component={Paper} className="table-container">
             <Table>
               <TableHead className="table-head">
                 <TableRow>
@@ -54,21 +95,34 @@ const Sales = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {performanceData.map((performance, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{performance.date.substring(0, 10)}</TableCell>
-                    <TableCell>{performance.state}</TableCell>
-                    <TableCell>{performance.city}</TableCell>
-                    <TableCell>{performance.event}</TableCell>
-                    <TableCell>{performance.sales.toLocaleString()}</TableCell>
-                    <TableCell>{performance.comment}</TableCell>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                  </TableRow>
-                ))}
+                {renderData()}
               </TableBody>
             </Table>
           </TableContainer>
+            <div className="pagination-container">
+              <IconButton onClick={handleFirstPage} disabled={currentPage === 1}>
+                <FirstPage />
+              </IconButton>
+              <IconButton onClick={handleClickPrev} disabled={currentPage === 1}>
+              <NavigateBefore />
+              </IconButton>
+              <span>{`${currentPage} / ${totalPage}`}</span>
+              <IconButton onClick={handleClickNext} disabled={currentPage === totalPage}>
+              <NavigateNext />
+              </IconButton>
+              <IconButton onClick={handleLastPage} disabled={currentPage === totalPage}>
+              <LastPage />
+              </IconButton>
+              <TextField
+              type="number"
+              variant="outlined"
+              size="small"
+              label="Oldal"
+              onChange={handleGoToPage}
+              inputProps={{min: 1, max: totalPage}}
+              />
+            </div>
+          </div>
         ) : (
           <p>No performance available.</p>
         )}
